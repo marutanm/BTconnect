65e2390c492cf3648f3f8487b5c4ff2a0b57cc43
marutanm <marutanm@gmail.com>
2010-07-19 11:53:01 +0900

    Added Shared/TitleViewController

---
+++ Shared/TitleViewController.h
@@ -0,0 +1,16 @@
+//
+//  TitleViewController.h
+//  BTconnect
+//
+//  Created by marutanm on 7/19/10.
+//  Copyright 2010 shisobu.in. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TitleViewController : UIViewController {
+
+}
+
+@end
---
+++ Shared/TitleViewController.m
@@ -0,0 +1,64 @@
+    //
+//  TitleViewController.m
+//  BTconnect
+//
+//  Created by marutanm on 7/19/10.
+//  Copyright 2010 shisobu.in. All rights reserved.
+//
+
+#import "TitleViewController.h"
+
+
+@implementation TitleViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+    NSLog(@"%s", __func__);
+    self.view = [[UIView alloc] init]; 
+}
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Overriden to allow any orientation.
+    return YES;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
